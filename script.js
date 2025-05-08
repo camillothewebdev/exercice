ccdf370aea56ac037038092082ac1d69e6df916b
camillothewebdev <camillothewebdev@gmail.com>
2025-05-08 15:02:20 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-const menu = document.querySelector('.menu');
-const navLinks = document.querySelector('.nav-links');
+const menu = document.querySelector(".menu");
+const navLinks = document.querySelector(".nav-links");
 
-menu.addEventListener('click', ()=>{
-    navLinks.classList.toggle('show');
-})
+menu.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
 
-window.addEventListener('scroll', ()=>{
-    navLinks.classList.remove('show');
-})
+window.addEventListener("scroll", () => {
+  navLinks.classList.remove("show");
+});
 
-document.querySelectorAll('.nav-links a').forEach(link => {
-    link.addEventListener('click', () => {
-        navLinks.classList.remove('show');
-    });
+document.querySelectorAll(".nav-links a").forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("show");
+  });
+});
+
+document.querySelectorAll(".dropdown").forEach((dropdown) => {
+    dropdown.querySelector("button").addEventListener("click", () => {
+    dropdown.classList.toggle("open");
+  });
 });
